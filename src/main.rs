9e0e80f7074b81f7a7f3290c82fc15e9a803bcdc
jm-connell <con18013@byui.edu>
2023-09-30 16:54:20 -0600

    added menu loop, separated into functions

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,35 @@
 use std::io;
-use std::io::{stdout, Write};
+use std::io::{Write};
 
 fn main() {
     println!("\n ----------------------------------\n| Caesar Cipher Encryption Program |\n ----------------------------------\n");
 
-    // Create variables
-    let mut message = String::new();
-    let mut key_string = String::new();
-    let mut key_int: u32 = 12;
+    // Display option menu
+    loop {
+        println!("Choose an option:");
+        println!("1. Encrypt message");
+        println!("2. Decrypt message");
+        println!("3. Quit");
+
+        let mut menu_choice: u32 = get_choice();
+
+        match menu_choice {
+            1 => { encrypt(); }
+            2 => { decrypt(); }
+            3 => {
+                println!(" --------\n| Quitting... |\n --------");
+                break
+            }
+            _ => println!("Invalid choice. Please choose 1, 2, or 3."),
+        }
+    }
+}
+
+
+fn encrypt() {
 
     // Get message to encrypt
+    let mut message = String::new();
     print!("Enter a secret message: ");
     match io::stdout().flush() {
         Ok(_) => (),
@@ -19,36 +39,12 @@ fn main() {
     println!("");
 
     // Get encryption key
-    print!("Enter the encryption key (positive integer): ");
-    match io::stdout().flush() {
-        Ok(_) => (),
-        Err(error) => println!("{}", error),
-    }
-    io::stdin().read_line(&mut key_string).expect("Error reading input");
-
-    // Parse key as u32 int, if error set to default of 12
-    let key_string: u32 = match key_string.trim().parse() {
-        Ok(key_int) => key_int,
-        Err(_) => {
-            println!("Key input is invalid. Default of 12 will be used.");
-            12
-        }
-    };
+    let mut key = get_key();
 
     // Encrypt message
-    let encrypted_message = encrypt(&message, key_int);
-    println!("\nOriginal Message:\n{}\nEncrypted Message:\n{}", message, encrypted_message);
-
-    let decrypted_message = decrypt(&encrypted_message, key_int);
-    println!("\nDecrypted Message:\n{}", decrypted_message);
-
-}
-
-
-fn encrypt(input: &str, key: u32) -> String {
     let mut output = String::new();
 
-    for c in input.chars() {
+    for c in message.chars() {
         if c.is_alphabetic() {
             let is_upper = c.is_uppercase();
             let offset = if is_upper { b'A' } else { b'a' };
@@ -59,13 +55,31 @@ fn encrypt(input: &str, key: u32) -> String {
         }
     }
 
-    output
+    // Display encrypted message
+    println!(" -------------------");
+    println!("| Encrypted Message |");
+    println!(" -------------------");
+    println!("{}", output);
 }
 
-fn decrypt(input: &str, key: u32) -> String {
+fn decrypt() {
+
+    // Get message to decrypt
+    let mut message = String::new();
+    print!("Enter a message to decrypt: ");
+    match io::stdout().flush() {
+        Ok(_) => (),
+        Err(error) => println!("{}", error),
+    }
+    io::stdin().read_line(&mut message).expect("Error reading input");
+    println!("");
+
+    // Get encryption key
+    let mut key = get_key();
+
     let mut output = String::new();
 
-    for c in input.chars() {
+    for c in message.chars() {
         if c.is_alphabetic() {
             let is_upper = c.is_uppercase();
             let offset = if is_upper { b'A' } else { b'a' };
@@ -75,5 +89,42 @@ fn decrypt(input: &str, key: u32) -> String {
             output.push(c);
         }
     }
-    output
+
+    // Display decrypted message
+    println!(" -------------------");
+    println!("| Decrypted Message |");
+    println!(" -------------------");
+    println!("{}", output);
+}
+
+fn get_choice() -> u32 {
+    loop {
+        let mut choice = String::new();
+        io::stdin().read_line(&mut choice).expect("Error reading input");
+        match choice.trim().parse() {
+            Ok(num) => return num,
+            Err(_) => println!("Invalid input. Please enter 1, 2, or 3."),
+        }
+    }
+}
+
+fn get_key() -> u32 {
+
+    let mut key_string = String::new();
+    print!("Enter the encryption key (positive integer): ");
+    match io::stdout().flush() {
+        Ok(_) => (),
+        Err(error) => println!("{}", error),
+    }
+    io::stdin().read_line(&mut key_string).expect("Error reading input");
+
+    // Parse key as u32 int, if error set to default of 12
+    let key_int: u32 = match key_string.trim().parse() {
+        Ok(key_int) => key_int,
+        Err(_) => {
+            println!("Key input is invalid. Default of 12 will be used.");
+            12
+        }
+    };
+    key_int
 }
